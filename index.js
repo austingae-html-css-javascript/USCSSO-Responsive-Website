982183133ca0b305ac4faf595da6a005f392224c
Austin Gae <austingae@gmail.com>
2022-06-15 16:43:13 -0700

    trying to fix blue screen

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-window.addEventListener("click", function(event) {
-  console.log(event.target.id);
-  if (event.target.id == "hamburger-logo") {
-    document.getElementById("hamburger-menu-page").style.cssText = "top: 0%; transition-property: top; transition-duration: 1s;";
-  }
-  else if (event.target.id == "x-mark") {
-    document.getElementById("hamburger-menu-page").style.cssText = "top: -100%; transition-property: top; transition-duration: 1s;";
-  }
+document.getElementById("hamburger-logo-button").addEventListener("click", function(event) {
+  document.getElementById("hamburger-menu-page").style.cssText = "top: 0%; transition-property: top; transition-duration: 1s;";
+});
+
+document.getElementById("x-mark").addEventListener("click", function(event) {
+  document.getElementById("hamburger-menu-page").style.cssText = "top: -100%; transition-property: top; transition-duration: 1s;";
 });
