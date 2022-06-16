db8d4204ec66c42839ac464bde7dc4cae7d87cbd
Austin Gae <austingae@gmail.com>
2022-06-15 17:03:04 -0700

    attempt to fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.getElementById("hamburger-logo-button").addEventListener("click", function(event) {
-  document.getElementById("hamburger-menu-page").style.cssText = "top: 0%; transition-property: top; transition-duration: 1s;";
+  document.getElementById("hamburger-menu-page").style.cssText = "left: 0%; transition-property: left; transition-duration: 1s;";
 });
 
 document.getElementById("x-mark").addEventListener("click", function(event) {
-  document.getElementById("hamburger-menu-page").style.cssText = "top: -100%; transition-property: top; transition-duration: 1s;";
+  document.getElementById("hamburger-menu-page").style.cssText = "left: -100%; transition-property: left; transition-duration: 1s;";
 });
